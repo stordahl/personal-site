b45b744992771e5f10e6759538649118a98e2f0b
Sanity.io <hello@sanity.io>
2021-12-16 22:49:52 -0600

    Initial commit

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,52 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-blog'
+      }
+    },
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '',
+                  title: 'Sanity Studio',
+                  name: 'personal-site-studio-3h19xtji',
+                  apiId: 'de579965-10ef-4213-be21-f6a34c0123a8'
+                },
+                {
+                  buildHookId: '',
+                  title: 'Blog Website',
+                  name: '',
+                  apiId: ''
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/stordahl/personal-site',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: '', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
